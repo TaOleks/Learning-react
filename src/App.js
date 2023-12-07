89e4f75ec81593e2cdd0e8250c096be88d77ddfc
TaOleks <tahtay.aleksey@gmail.com>
2023-12-07 19:54:23 +0100

    PostItem component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,23 @@
 
 import React from "react"
-import { useState } from "react";
-import Counter from "./components/Counter";
-import ClassCounter from "./components/ClassCounter";
+import './styles/App.css'
+import PostItem from "./components/PostItem";
 
 function App() {
-  
-  const [value, setValue] = useState('Text in input')
- 
-  
-  
+
+
+
+
   return (
-    
-    
+
+
     <div className="App">
-      
-      <h2>{value}</h2>
-      <input 
-      type="text"
-      value={value}
-      onChange={(event) =>setValue(event.target.value)}
-      />
-    
-    <Counter/>
-    <ClassCounter/>
-    
+      <PostItem/>
+      <PostItem/>
+      <PostItem/>
+      <PostItem/>
+
+
     </div>
   );
 }
---
+++ src/components/PostItem.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function PostItem() {
+  return (
+    <div>
+        <div className="post">
+    <div className="post-content">
+      <strong>1.Javascript</strong>
+      <div>
+        Javascript - is a programming language
+      </div>
+    </div>
+    <div className="post-btns">
+      <button>Delete</button>
+
+    </div>
+
+  </div>
+  </div>
+  )
+}
+
+export default PostItem
