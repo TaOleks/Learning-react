eda0e1e6aa7c462c0523869750bac98215ae272a
TaOleks <tahtay.aleksey@gmail.com>
2023-12-08 12:18:39 +0100

    Form . Don't controllable component
    using  UseRef()

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import React from "react"
 import './styles/App.css'
-
+import { useRef } from "react";
 import { useState } from "react";
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton ";
@@ -15,12 +15,16 @@ const [posts, setPosts] = useState([
 ])
 
 const [title, setTitle] = useState('')
+
+const bodyInputRef = useRef()
+
 const addNewPost = (e) =>{
   e.preventDefault()
 console.log(title)
+
+console.log(bodyInputRef.current.value)
+
 }
-
-
 
   return (
 
@@ -34,8 +38,17 @@ console.log(title)
         onChange={e => setTitle(e.target.value) }
          type='text'
           placeholder='Post name' />
-        <MyInput type='text' placeholder='Post describtion' />
+
+          {/* Don't controllable component */}
+        <MyInput
+        ref={bodyInputRef}
+         type='text' 
+         placeholder='Post describtion' />
         <MyButton onClick={addNewPost} >Create post</MyButton>
+
+        {/* <input
+        ref={bodyInputRef}
+        /> */}
       </form>
 
     <PostList posts={posts} title='List of post 1'/>  
