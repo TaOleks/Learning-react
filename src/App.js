7b48d4c9e13e3952e298592ede8c79a42707ed13
TaOleks <tahtay.aleksey@gmail.com>
2023-12-08 11:41:01 +0100

    Create form
    1 input.module
    2 button module
    3 work with props, children

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 
 import React from "react"
 import './styles/App.css'
-import PostItem from "./components/PostItem";
+
 import { useState } from "react";
 import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton ";
+import MyInput from './components/UI/input/MyInput'
 
 function App() {
 const [posts, setPosts] = useState([
@@ -11,16 +13,7 @@ const [posts, setPosts] = useState([
 {id:2, title:'Javascript 2', body:'Description my post'},
 {id:3, title:'Javascript 3', body:'Description my post'}
 ])
-const [posts2, setPosts2] = useState([
-  {id:1, title:'Java 1', body:'Description Java post'},
-  {id:2, title:'Java 2', body:'Description Java post'},
-  {id:3, title:'Java 3', body:'Description Java post'}
-  ])
-  const [posts3, setPosts3] = useState([
-    {id:1, title:'Pyton 1', body:'Description Pyton post'},
-    {id:2, title:'Pyton 2', body:'Description Pyton post'},
-    {id:3, title:'Pyton 3', body:'Description Pyton post'}
-    ])
+
 
 
 
@@ -30,12 +23,15 @@ const [posts2, setPosts2] = useState([
 
     <div className="App">
 
+      <form>
+        <MyInput type='text' placeholder='Post name' />
+        <MyInput type='text' placeholder='Post describtion' />
+        <MyButton disabled >Create post</MyButton>
+      </form>
+
     <PostList posts={posts} title='List of post 1'/>  
 
-    <PostList posts={posts2} title='List of post 2'/>  
-    
-    <PostList posts={posts3} title='List of post 3'/>  
-
+   
     
     </div>
   );
