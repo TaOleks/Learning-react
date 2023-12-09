a7c3d2f377a9c3dfba8a1716801f8c84c9b2a43e
TaOleks <tahtay.aleksey@gmail.com>
2023-12-09 19:08:11 +0100

    Create Searching
    useMemo()

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 
-import React from "react"
+import React, { useMemo } from "react"
 import './styles/App.css'
 import { useState } from "react";
 import PostList from "./components/PostList";
 
 import PostForm from "./components/UI/PostForm";
 import MySelect from "./components/UI/select/MySelect";
-
+import MyInput from "./components/UI/input/MyInput";
 
 
 
 function App() {
   const [posts, setPosts] = useState([
-    { id: 1, title: 'dddddddddd 1', body: 'kkkkkkkkk 1' },
-    { id: 2, title: 'jjjjjjjj 2', body: 'ddddddddddddd 2' },
-    { id: 3, title: 'aaaaaaaaaa 3', body: 'bbbbbbb 3' }
+    { id: 1, title: 'dd 1', body: 'kkkkkkkkk 1' },
+    { id: 2, title: 'djj 2', body: 'ddddddddddddd 2' },
+    { id: 3, title: 'aa 3', body: 'bbbbbbb 3' }
   ])
 
   const [selectedSort, setSelectedSort] = useState('')
+  const [searchQuery, setSearchQuery] = useState ('')
+  
+
+
+  const sortedPosts = useMemo(() => {
+    
+    if(selectedSort){
+      return [...posts].sort((a,b)=> a[selectedSort].localeCompare(b[selectedSort]))
+    }
+    return posts;
+  }, [selectedSort, posts]);
+
+  const sortedAndSearchedPosts = useMemo(() =>{
+    return sortedPosts.filter(post =>post.title.toLowerCase().includes(searchQuery))
+  }, [searchQuery, sortedPosts])
+  
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
   }
@@ -25,9 +41,10 @@ function App() {
   const removePost = (post) => {
     setPosts(posts.filter(p => p.id !== post.id))
   }
+
   const sortPosts = (sort)=>{
     setSelectedSort(sort)
-    setPosts([...posts].sort((a,b)=> a[sort].localeCompare(b[sort])))
+    
     
   }
 
@@ -37,6 +54,11 @@ function App() {
       <PostForm create={createPost} />
       <hr style={{margin: '15px'}}/>
       <div>
+        <MyInput
+        value={searchQuery}
+        onChange= {e =>setSearchQuery(e.target.value)}
+        placeholder="Search..."
+        />
         <MySelect
         value={selectedSort}
         onChange={sortPosts}
@@ -48,9 +70,9 @@ function App() {
         />
       </div>
 
-      {posts.length
+      {sortedAndSearchedPosts.length
         ?
-        <PostList remove={removePost} posts={posts} title='List of post 1' />
+        <PostList remove={removePost} posts={sortedAndSearchedPosts} title='List of post 1' />
         :
         <h1
           style={{ textAlign: 'center' }}> You don't have  posts  at all
