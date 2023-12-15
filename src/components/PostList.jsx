80a0a8d013dc4d793d82d1c9d7b1e57c8e3b4cab
TaOleks <tahtay.aleksey@gmail.com>
2023-12-15 11:50:27 +0100

    Animation PostList
     1. instal and use React Translation Group
     (TranslationGroup, CSSTranslation)

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,24 +1,35 @@
 import React from 'react'
 import PostItem from './PostItem'
+import {TransitionGroup, CSSTransition} from 'react-transition-group'
 
-function PostList({posts,  title, remove}) {
 
-  if(!posts.length){
-    return(
-      
-        <h1
-          style={{ textAlign: 'center' }}> You don't have  posts  at all
-        </h1>
-      
+function PostList({ posts, title, remove }) {
+
+  if (!posts.length) {
+    return (
+
+      <h1
+        style={{ textAlign: 'center' }}> You don't have  posts  at all
+      </h1>
+
     )
   }
 
 
   return (
     <div>
-        <h1 style={{textAlign:'center'}}> {title}</h1>
-      {posts.map((post, index) =>
-        <PostItem remove={remove} number={index+1} post ={post} key={post.id}  />)}
+      <h1 style={{ textAlign: 'center' }}> {title}</h1>
+      <TransitionGroup>
+        {posts.map((post, index) =>
+          <CSSTransition
+            key={post.id}
+            timeout={500}
+            classNames="post">
+            <PostItem remove={remove} number={index + 1} post={post}  />
+          </CSSTransition>
+        )}
+      </TransitionGroup>
+
     </div>
   )
 }
