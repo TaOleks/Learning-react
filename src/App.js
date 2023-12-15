83b9226166fedc0df7715897acc5f5039f33a31a
TaOleks <tahtay.aleksey@gmail.com>
2023-12-15 10:55:04 +0100

    Modal Box continue

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import PostList from "./components/PostList";
 import PostForm from "./components/UI/PostForm";
 import PostFilter from "./components/UI/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
-
+import MyButton from "./components/UI/button/MyButton ";
 
 
 function App() {
@@ -17,7 +17,7 @@ function App() {
   ])
 
   const [filter, setFilter] = useState({sort:'', query:''})
-  
+  const [modal, setModal] = useState(false)
 
 
   const sortedPosts = useMemo(() => {
@@ -34,6 +34,7 @@ function App() {
   
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
+    setModal(false)
   }
 
   const removePost = (post) => {
@@ -44,7 +45,10 @@ function App() {
   return (
 
     <div className="App">
-      <MyModal visible = {true}>
+      <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
+        Create user
+      </MyButton>
+      <MyModal visible = {modal} setVisible={setModal}>
         
         <PostForm create={createPost} />
       </MyModal>
