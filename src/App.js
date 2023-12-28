06d70dac614cea2fc3461c56a2209ef686b9e28d
TaOleks <tahtay.aleksey@gmail.com>
2023-12-28 12:40:29 +0100

    Apdata routing
    Link to

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 import './styles/App.css'
 import React from 'react'
 import About from './pages/About'
@@ -7,10 +7,18 @@ import Posts from './pages/Posts'
 
 function App() {
   return (
-   // <About></About>
-    //<Posts></Posts>
-    
+   
       <BrowserRouter>
+
+      <div className='navbar'>
+        <div className='navbar__links'>
+       {/* bad practice */}
+       {/* <a href='/posts'>Posts </a> */}
+        {/* Good practice */}
+          <Link to='/about'>About website </Link>
+          <Link to='/posts'>Posts </Link>
+        </div>
+      </div>
       <Routes>
         <Route path="/about" element={<About/>}/> 
           
