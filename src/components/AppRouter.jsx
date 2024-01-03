42c821f1353b5c45576527c331df16bf05b3d1c6
TaOleks <tahtay.aleksey@gmail.com>
2024-01-03 10:56:30 +0100

    Decompozition Routing

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,20 +1,31 @@
+
 import React from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
-import About from '../pages/About'
-import Posts from '../pages/Posts'
-import Errors from '../pages/Errors'
-import PostIdPage from '../pages/PostIdPage'
+// import About from '../pages/About'
+// import Posts from '../pages/Posts'
+// import Errors from '../pages/Errors'
+// import PostIdPage from '../pages/PostIdPage'
+import { routes } from '../router/rout'
 
 function AppRouter() {
   return (
     
     <Routes>
-     <Route path="/about" element={<About/>}/>  
+        {routes.map(route =>
+        <Route 
+          element={<route.element/>}
+            path={route.path}
+            exact={route.exact}
+          />
+          
+        )}
+      
+     {/* <Route path="/about" element={<About/>}/>  
      <Route exact path="/posts" element={<Posts/>}/>
      <Route path="/error" element={<Errors/>}/>
-     <Route exact path="/posts/:id" element={<PostIdPage/>}/>
+     <Route exact path="/posts/:id" element={<PostIdPage/>}/> */}
      
-     <Route path="*" element={<Navigate to="/error"/>}/>
+     <Route path="*" element={<Navigate to="/posts"/>}/>
    </Routes>
   )
 }
